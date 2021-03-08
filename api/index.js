2339d29fc3de790a9b01419df03a6a142c521e37
lynxyan <lynxyan@tencent.com>
2021-03-08 21:37:33 +0800

    feat: 用户信息卡片

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
-import { requetUrl } from '../utils/constants';
-console.log(requetUrl);
+const doNothing = () => {};
 
-export const a = requetUrl;
+const api = {
+	getSession: ({success = doNothing, fail = doNothing, ...data} = {}) => wx.request({
+		url: 'https://api.weixin.qq.com/sns/jscode2session',
+		method: 'GET',
+		data: {
+			appid: 'wx6c7ec139de7d2240',
+			secret: '4c548790f7f9df8f2326cac317c299f2',
+			grant_type: 'authorization_code',
+			js_code: data.code,
+		},
+		success,
+		fail,
+	}),
+}
+
+export default api;
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,5 @@
+const app = getApp();
+
 Page({
 	data: {
 		list: [{
@@ -16,10 +18,36 @@ Page({
 			selectedIconPath: '/assets/icons/user_filled.png',
 		}],
 		currentIndex: 0,
+		profileUrl: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1986451467,394304688&fm=26&gp=0.jpg',
+		isLogin: 'false',
+	},
+	onLoad() {
+		this.setData({
+			userInfo: app.globalData.userInfo,
+			isLogin: !!app.globalData.userInfo,
+		});
 	},
 	tabChange(e) {
 		this.setData({
 			currentIndex: e.detail.index,
 		});
-	}
+	},
+	imageLoad: function(e) {
+		const $width=e.detail.width,    //获取图片真实宽度
+			$height=e.detail.height,
+			ratio=$width/$height;   //图片的真实宽高比例
+		const viewWidth=150,           //设置图片显示宽度，
+			viewHeight=150/ratio;    //计算的高度值   
+		this.setData({
+			imgwidth:viewWidth,
+			imgheight:viewHeight
+		});
+	},
+	getUserInfo(e) {
+		app.globalData.userInfo = e.detail.userInfo;
+		this.setData({
+			userInfo: e.detail.userInfo,
+			isLogin: true,
+		});
+	},
 });
